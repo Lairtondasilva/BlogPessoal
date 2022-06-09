00dacc5faa9650581381b43deda41d4317746a98
unknown <lairtondasilva07@gmail.com>
2022-06-08 22:45:52 -0300

    adicionando put-get-delete em usuarioController

--- src/main/java/org/generation/blogPessoal/controller/UsuarioController.java
+++ src/main/java/org/generation/blogPessoal/controller/UsuarioController.java
@@ -1,17 +1,23 @@
 package org.generation.blogPessoal.controller;
 
+import java.util.List;
 import java.util.Optional;
 
 import javax.validation.Valid;
 
 import org.generation.blogPessoal.model.Usuario;
 import org.generation.blogPessoal.model.UsuarioLogin;
+import org.generation.blogPessoal.repository.UsuarioRepository;
 import org.generation.blogPessoal.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +30,9 @@ public class UsuarioController {
 	@Autowired
 	private UsuarioService usuarioService;
 	
+	@Autowired
+	private UsuarioRepository repository;
+	
 	@PostMapping("/logar")
 	public ResponseEntity<UsuarioLogin> autentication(@RequestBody Optional<UsuarioLogin> user){
 		return usuarioService.Logar(user).map(usuario->ResponseEntity.ok(usuario))
@@ -34,5 +43,21 @@ public class UsuarioController {
 	public ResponseEntity<Usuario> Post (@Valid @RequestBody Usuario user){
 		return ResponseEntity.status(HttpStatus.CREATED)
 				.body(usuarioService.cadastrarUsuario(user));
+	}
+	
+	//implementar depois
+	@PutMapping
+	public ResponseEntity<Usuario> atualizar (@RequestBody Usuario user){
+	 return null;
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<Usuario>> pegarTodos(){
+		return ResponseEntity.ok(repository.findAll());
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deletar(@PathVariable Long id) {
+		repository.deleteById(id);
 	}
 }
